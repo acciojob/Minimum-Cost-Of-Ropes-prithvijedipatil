5220901082d17fa2b70f838165dcf97fa733bd5b
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-13 13:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-function calculateMinCost() {
-  //your code here
+
+
+  let x= document.getElementById('rope-lengths');
+  let str = x.value;
+  let arr = str.split(",");
+
+  function calculateMinCost(arr) {
+    //your code here
+    let arr1=[];
+    let price=[];
+    arr.sort((a,b)=>{
+        if(a>b){
+            return 1;
+        }
+        if(b>a){
+            return -1;
+        }
+        if(a==b){
+            return 0;
+        }
+    })
+
+    while(arr.length>1){
+
+        let res = arr[0]+arr[1];
+        arr1.unshift(res);
+        price.push(res);
+        for(let i=2;i<arr.length;i++){
+            arr1.push(arr[i]);
+        }
+        arr = [...arr1];
+        arr1.length=0;
+
+        
+    }
+    let final=0;
+
+    for(let j=0;j<price.length;j++){
+        final = final + price[j];
+    }
+
+    return final;
+
+    
+    
+  }  
+
+
   
-  
-  
-}  
