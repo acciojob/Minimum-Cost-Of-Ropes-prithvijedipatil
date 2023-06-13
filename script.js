9316e3c0efac4e41423b66188ff5a302670360dc
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-13 14:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 
 
-  let x= document.getElementById('rope-lengths');
-  let str = x.value;
-  let arr = str.split(",");
+  
 
-  let z = function calculateMinCost(arr) {
+  function calculateMinCost() {
+
+    let x= document.getElementById('rope-lengths');
+  let str = x.value;
+  let arr3= str.split(",");
+  let arr = arr3.map(Number);
+ 
     //your code here
+    
     let arr1=[];
-    let price=[];
+        let price=[];
+        arr.sort((a,b)=>{
+            if(a>b){
+                return 1;
+            }
+            if(b>a){
+                return -1;
+            }
+            if(a==b){
+                return 0;
+            }
+        })
+   
+    let len = arr.length-1;
+        while(len>0){
+    
+            let res = parseInt(arr[0])+parseInt(arr[1]);
+            arr1.push(res);
+            price.push(res);
+            for(let i=2;i<arr.length;i++){
+                arr1.push(arr[i]);
+            }
+            arr = [...arr1];
+            arr1.length=0;
+    len--;
+  
     arr.sort((a,b)=>{
         if(a>b){
             return 1;
@@ -19,32 +49,21 @@
             return 0;
         }
     })
-
-    while(arr.length>1){
-
-        let res = arr[0]+arr[1];
-        arr1.unshift(res);
-        price.push(res);
-        for(let i=2;i<arr.length;i++){
-            arr1.push(arr[i]);
+            
         }
-        arr = [...arr1];
-        arr1.length=0;
-
-        
-    }
-    let final=0;
-
-    for(let j=0;j<price.length;j++){
-        final = final + price[j];
-    }
-
-    return final;
-
+    console.log(price);
+        let final=0;
     
+        for(let j=0;j<price.length;j++){
+            final = final + price[j];
+        }
+    
+        document.getElementById('result').innerHTML=final;
+    
+        
     
   }  
 
-  document.getElementById('result').value=z;
+
 
   
